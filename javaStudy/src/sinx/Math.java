187d6b74927118da322c327d7cfa2c8f5f2bcbe4
s1nd <1172358481@qq.com>
2019-09-11 12:46:38 +0800

    sinx

---
+++ javaStudy/src/sinx/Math.java
@@ -0,0 +1,24 @@
+package sinx;
+
+public class Math {
+	Math(){
+		
+	}
+
+	private double sinx(double x){
+		double item =x;
+		double result=x;
+		for(int i=2;i<100;i++) {
+			item *= (-1)*(x*x)/((2*i-2)*(2*i-1));
+			System.out.println("item:"+item);
+			result +=item;
+		}
+		return result;
+	}
+	public static void main(String []args) {
+		Math s = new Math();
+		double pi = 3.1415926;
+		double p = s.sinx(pi/6);
+		System.out.println(p);
+	}
+}
